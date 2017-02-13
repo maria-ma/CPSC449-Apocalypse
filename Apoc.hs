e27e5712bf9400935a1bdec0367a8dbbd9f6fa87
Maria Mamaclay <maria@Marias-MacBook-Pro.local>
2017-02-13 12:06:47 -0700

    moves ai functions into strategy random

--- Apoc.hs
+++ Apoc.hs
@@ -163,42 +163,6 @@ checkKnightLegal board player (fromX, fromY) to
    | otherwise = False
    where checkTo = ((checkEmptySpace board to) || (checkOpponent board player to))
 
----AI Movement functions-----------------------------------------------------------
-
--- | getPieces: gets list of playable pieces based on the player
---   params: the board, the player type
---   returns: list of tupbles indicating playable start sources
-getPieces :: Board -> Player -> [(Int, Int)]
-getPieces board White = getBoardPieces (\x -> x == WP || x == WK) board 0
-getPieces board Black = getBoardPieces (\x -> x == BP || x == BK) board 0
-
--- | getBoardPieces: gets all available board pieces
---   input: boolean function indicating which board piece is playable (white or black pieces), the board, starting row number
---   returns: list of tuples of playable pieces
-getBoardPieces :: (a -> Bool) -> [[a]] -> Int -> [(Int, Int)]
-getBoardPieces func [] _ = []
-getBoardPieces func (x:xs) row = (map (\x -> (x, row)) $ getRowPieces func x 0) ++ (getBoardPieces func xs (row + 1))
-
--- | getRowPieces: gets all playable pieces in a specific row (traverses through the column)
---   input: boolean function called from getBoardPieces, cell from board, (int) current column number
-getRowPieces :: (a -> Bool) -> [a] -> Int -> [Int]
-getRowPieces func [] _ = []
-getRowPieces func (x:xs) column = if func x then column:(getRowPieces func xs (column + 1)) else getRowPieces func xs (column + 1)
-
--- | getMoveList: gets the list of valid moves based on the current player's piece
-getMoveList :: Board -> Player -> (Int, Int) -> [(Int, Int)]
-getMoveList board player (fromX, fromY)
-    -- * player is currently playing a pawn
-    | (startPiece == WP) || (startPiece == BP) = 
-        [(fromX, fromY + forward), (fromX - 1, fromY + forward), (fromX + 1, fromY + forward)]
-    -- * otherwise, player is currently playing a knight
-    | otherwise = 
-        [(fromX + 1, fromY + 2), (fromX + 1, fromY - 2), (fromX - 1, fromY - 2), (fromX -1, fromY +2), (fromX + 2, fromY + 1), (fromX + 2, fromY - 1), (fromX -2, fromY - 1), (fromX - 2, fromY + 1)]
-    where startPiece = getFromBoard board (fromX, fromY)
-          forward = case player of
-                        Black -> -1
-                        White -> 1
-
 ---Player Strategy functions-------------------------------------------------------
 
 greedy :: Chooser
